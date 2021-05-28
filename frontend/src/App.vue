ed1095f0d15820abf9ec0cd192775ad9c237172b
Noer Paanakker <noer@lightworksweb.nl>
2021-05-28 17:17:15 +0200

    componentized navbar and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,31 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">
-          <strong>Djackets</strong>
-        </router-link>
-        <a
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar-menu"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div class="navbar-menu" id="navbar-menu">
-        <div class="navbar-end">
-          <router-link to="/summer" class="navbar-item">Summer</router-link>
-          <router-link to="/winter" class="navbar-item">Winter</router-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <router-link to="/login" class="button is-light"
-                >Login</router-link
-              >
-              <router-link to="/cart" class="button is-success">
-                <span class="icon"><i class="fas fa-shopping-cart"></i></span
-                ><span>Cart</span>
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <section class="section">
       <router-view />
     </section>
 
-    <footer class="footer">
-      <p class="has-text-centered">Copyright &copy; 2021</p>
-    </footer>
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
+});
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
 </style>
+
+
--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright &copy; 2021</p>
+  </footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "About",
+  name: "Footer",
   components: {},
 });
 </script>
